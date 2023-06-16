f2ec187858fa68cf81a1c9c891254ca0260fd873
Deepak Suthar <codewithdeepak@yahoo.com>
2023-06-16 09:39:15 +0530

    feat: getAllOrders service

---
+++ src/services/getAllOrders.js
@@ -0,0 +1,43 @@
+
+
+e
+
+import { collection, getDocs, query } from 'firebase/firestore'
+
+import { fireDb } from '../firebase/client'
+import { Models } from '../firebase/models'
+
+
+
+
+export const getAllOrders = async () => {
+    const failedResponse = {
+        success: false,
+        message: '',
+        orders: undefined
+    }
+
+    const queries = query(
+        collection(fireDb, `${Models.ORDERS}`),
+    )
+
+    try {
+        const docsSnap = await getDocs(queries)
+        if (docsSnap.empty) {
+            failedResponse.message = 'orders does not exist'
+            return failedResponse
+        }
+
+        const metaData = docsSnap.docs.map((item) => {
+            return item.data()
+        })
+
+        const products = metaData
+        failedResponse.success = true
+        failedResponse.orders = orders
+        return failedResponse
+    } catch (error) {
+        failedResponse.message = error.toString()
+        return failedResponse
+    }
+}
