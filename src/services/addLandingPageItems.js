7a5fb0f369efb5f46d0108d0c52bde3a24f5ecae
Deepak Suthar <codewithdeepak@yahoo.com>
2023-06-18 16:14:11 +0530

    fix

--- src/services/addLandingPageItems.js
+++ src/services/addLandingPageItems.js
@@ -27,8 +27,7 @@ export default async function addLandingPageItems(item) {
 
     return Promise.all(imageUrls).then(async (data) => {
 
-        console.log(data)
-        const updatedProduct = { productId: item.productId, id: itemId, ...(mode === 'IMAGE' ? { primaryImageWeb: data[0], primaryImageMobile: data[1] } : { primaryVideoWeb: data[0], primaryVideoMobile: data[1] }) }
+        const updatedProduct = { productId: item.productId, mode: item.mode, id: itemId, ...(mode === 'IMAGE' ? { primaryImageWeb: data[0], primaryImageMobile: data[1] } : { primaryVideoWeb: data[0], primaryVideoMobile: data[1] }) }
         const landingItemRef = doc(fireDb, Models.LANDING_PAGE_ITEM, itemId);
         const productRef = doc(fireDb, Models.PRODUCTS, item.productId);
         await setDoc(landingItemRef, updatedProduct);
