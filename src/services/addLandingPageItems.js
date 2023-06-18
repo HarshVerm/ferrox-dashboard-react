cc0d6af48cc51b630afb2767fa2ea04c1ef2e66d
Deepak Suthar <codewithdeepak@yahoo.com>
2023-06-18 15:19:21 +0530

    fixed services

--- src/services/addLandingPageItems.js
+++ src/services/addLandingPageItems.js
@@ -9,11 +9,11 @@ export default async function addLandingPageItems(item) {
         return { success: false, message: 'Payload missing some values' }
     }
 
-    const { primaryImage, primaryVideo, productId } = item
+    const { primaryImageWeb, primaryImageMobile, primaryVideoWeb, primaryVideoMobile, productId, mode } = item
 
     const itemId = uuidv4()
 
-    const uploadItems = [primaryImage, primaryVideo]
+    const uploadItems = mode === 'IMAGE' ? [primaryImageWeb, primaryImageMobile] : [primaryVideoWeb, primaryVideoMobile]
 
     const imageUrls = uploadItems.map(async (set, _set_index) => {
         //NOTE: in case of file path is provided, uncomment this and pass data
@@ -23,7 +23,7 @@ export default async function addLandingPageItems(item) {
     })
 
     Promise.all(imageUrls).then(async (data) => {
-        const updatedProduct = { ...item, id: itemId, primaryImage: data[0], primaryVideo: data[1] }
+        const updatedProduct = { ...item, id: itemId, ...(mode === 'IMAGE' ? { primaryImageWeb: data[0], primaryImageMobile: data[1] } : { primaryVideoWeb: data[0], primaryVideoMobile: data[1] }) }
         const landingItemRef = doc(fireDb, Models.LANDING_PAGE_ITEM, itemId);
         const productRef = doc(fireDb, Models.PRODUCTS, item?.productId);
         await setDoc(landingItemRef, updatedProduct);
